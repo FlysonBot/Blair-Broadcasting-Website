c7d8aef34ac70ba23fd1e2be468e9a55b633de0c
FlysonBot <116744100+FlysonBot@users.noreply.github.com>
2022-10-27 08:26:52 -0700

    Update survey.tsx
    Change the survey URL from embedded to the original.

--- pages/survey.tsx
+++ pages/survey.tsx
@@ -17,7 +17,7 @@ export default function Survey() {
     return (
       <>
       <Center>
-        <iframe className={classes.formFrame} style={{ height: "1600px"}} allowFullScreen={true} sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-popups-to-escape-sandbox allow-downloads allow-modals" frameBorder="0" aria-label="Google Forms, Broadcast Video Survey"  src="https://docs.google.com/forms/d/e/1FAIpQLSfxDrqF6ZMJl7IgMRMzFcH9X0iBsH2nAWrd75xKTi8gmbIqNw/viewform?embedded=true"></iframe>
+        <iframe className={classes.formFrame} style={{ height: "1600px"}} allowFullScreen={true} sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-popups-to-escape-sandbox allow-downloads allow-modals" frameBorder="0" aria-label="Google Forms, Broadcast Video Survey"  src="https://docs.google.com/forms/d/e/1FAIpQLSfxDrqF6ZMJl7IgMRMzFcH9X0iBsH2nAWrd75xKTi8gmbIqNw/viewform"></iframe>
       </Center>
       </>
     );
